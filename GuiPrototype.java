4856c92811784f16e446d974a19f57f605aecb02
Alejandro Torres <aleth2k@gmail.com>
2023-10-28 23:07:21 -0600

    ya es funcional, solo hace la funcion numero 1 falta agregar todo lo demas, sobretodo en el codigo func agregar la parte de los archivos de texto, ya ultimo cambio porque ya me   aburri xd

--- GuiPrototype.java
+++ GuiPrototype.java
@@ -2,20 +2,288 @@ import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.plaf.BorderUIResource;
 import javax.swing.plaf.basic.BasicScrollBarUI;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.time.LocalTime;
+import java.util.Random;
 
 public class GuiPrototype extends JFrame implements ActionListener{
-    
+    public class Carro {
+        int numeroControl;
+        String color;
+        String marca;
+        String modelo;
+        int monto;
+        String nombreComprador;
+        int edad;
+        char metodoPago;
+        double montoEnganche;
+        double liquidacion;
+        String mensualidad;
+        /*gets */
+        public int getNumeroControl() {
+            return numeroControl;
+        }
+        public String getColor() {
+            return color;
+        }
+        public String getMarca() {
+            return marca;
+        }
+        public String getModelo() {
+            return modelo;
+        }
+        public int getMonto() {
+            return monto;
+        }
+        /*sets */
+        public void setNumeroControl(int numeroControl) {
+            this.numeroControl = numeroControl;
+        }
+        public void setColor(String color) {
+            this.color = color;
+        }
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+        public void setMonto(int monto) {
+            this.monto = monto;
+        }
+    }
+
     JButton btn;
     JLabel jLabel1;
     JLabel jLabel2;
     JPasswordField jPasswordField1;
     JTextField jTextField1;
     Border lineBorder = new LineBorder(Color.RED, 1);
-    int i = 0;
+
+    public static int[] array = new int[10];
+    public static int[] mesesAdeudo = new int[10];
+    public static int i, l, valores = 9, a = 0, inicio, nuevoValor, valorEliminar, posicion;
+    public static char eliminar;
+    public static String[] color = {"Blanco", "Negro", "Azul", "Rojo", "Verde", "Amarillo", "Plata", "Gris", "Rosa", "Naranja"};
+    public static String[] marca = {"Audi", "BMW", "MercedesBenz", "Mclaren", "Lamborghini", "Toyota", "Chevrolet", "Nissan", "Mazda", "Renoult"};
+    public static String[] modelo = {"A1", "A2", "A3", "A4", "Aventador", "A6", "A7", "A8", "A9", "A10"};
+    public static int[] monto = {1087000, 1000000, 900000, 800000, 600000, 500000, 400000, 300000, 200000, 100000};
+    public static String[] nombreComprador = new String[10];
+    public static int[] edad = new int[10];
+    public static String[] metodoPago = new String[10];
+    public static double[] montoEnganche = new double[10];
+    public static double[] liquidacion = new double[10];
+    public static double[] adeudo = new double[10];
+    public static double[] pagoPorMes = new double[10];
+    
+    public static void valoresAleatorios() {
+        Random rand = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(9000) + 1000;
+        }
+    }
+
+    public static String[] mostrarDatos() {
+        String[] mostrarDatosStr = new String[10];
+        valoresAleatorios();
+        //System.out.print("\n\tInventario:\n");
+
+        for (int i = 0; i <= valores; i++) {
+            mostrarDatosStr[i] = (String.format("Carro no.%d:\nColor: %s\nMarca: %s\nModelo: %s\nPrecio: %d", array[i], color[i], marca[i], modelo[i], monto[i]));
+        }
+
+        //System.out.print("\n\n");
+        return mostrarDatosStr;
+    }
+
+    public static void mostrarDatosSeleccionados(Carro carro) {
+        System.out.print("\nElemento seleccionado:\n");
+        System.out.print(String.format("Carro no.%d:\nColor: %s\nMarca: %s\nModelo: %s\nPrecio: %d\n", carro.getNumeroControl(), carro.getColor(), carro.getMarca(), carro.getModelo(), carro.getMonto()));
+        System.out.print("\n\n");
+    }
+
+    public static void mostrarCarrosVendidos(Carro[] carrosVendidos, int numCarrosVendidos) {
+
+        System.out.print("\nCarros vendidos hasta el momento:\n");
+        for (int i = 0; i < numCarrosVendidos; i++) {
+            System.out.print(String.format("\n\t\tVenta no.%d\n\nComprador: %s\nCarro no.%d:\nColor: %s\nMarca: %s\nModelo: %s\nPrecio: %d\n", i + 1, nombreComprador[i], carrosVendidos[i].getNumeroControl(), carrosVendidos[i].getColor(), carrosVendidos[i].getMarca(), carrosVendidos[i].getModelo(), carrosVendidos[i].getMonto()));
+        }
+
+        System.out.print("\n\n");
+    }
+
+
+    public static void mostrarDatosComprador(Carro[] compradores, int numCompradores) {
+        for (int i = 0; i < numCompradores; i++) {
+            System.out.print(String.format("\n\nComprador %d: %s", i + 1, nombreComprador[i]));
+            System.out.print(String.format("\nEdad: %d", edad[i]));
+            System.out.print(String.format("\nMétodo de pago: %s", metodoPago[i]));
+
+            if (montoEnganche[i] == liquidacion[i]) {
+                System.out.print(String.format("\nSe realizará el pago completo: %.2f", liquidacion[i]));
+            } else {
+                System.out.print(String.format("\nMonto de Enganche: %.2f", montoEnganche[i]));
+                System.out.print(String.format("\nAdeudo: %.2f", adeudo[i]));
+            }
+
+            System.out.print(String.format("\nModelo solicitado: %s %s", compradores[i].getMarca(), compradores[i].getModelo()));
+        }
+        System.out.print("\n\n");
+    }
+
+    public static void mostrarInfoPago(int numCompradores) {
+        for (int i = 0; i < numCompradores; i++) {
+            System.out.print(String.format("\n\nComprador %d: %s", i + 1, nombreComprador[i]));
+            System.out.print(String.format("\nMétodo de pago: %s", metodoPago[i]));
+
+            if (montoEnganche[i] == liquidacion[i]) {
+                System.out.print(String.format("\nSe realizará el pago completo: %.2f", liquidacion[i]));
+            } else {
+                System.out.print(String.format("\nMonto de Enganche: %.2f", montoEnganche[i]));
+                System.out.print(String.format("\nAdeudo: %.2f", adeudo[i]));
+                System.out.print(String.format("\nPlazo: %d años \nMensualidad: $%.2f", mesesAdeudo[i], pagoPorMes[i]));
+            }
+        }
+        System.out.print("\n\n");
+    }
+
+    public static boolean buscar(Carro carroSeleccionado, Carro[] carrosVendidos, int numCarrosVendidos, Carro[] compradores, int numCompradores, int numBtn) {
+        do {
+            int valorEliminar = 0;
+            int truE = 0;
+            String input = JOptionPane.showInputDialog(null, "Selecciona un carro a través de su número de control: ");
+
+            try {
+                valorEliminar = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada no válida. Introduce un número de control válido.");
+                continue;
+            }
+
+            for (int i = 0; i <= valores; i++) {
+                if (array[i] == valorEliminar && valorEliminar == numBtn) {
+                    truE = 1;
+                    a = 1;
+                    inicio = i;
+                    registroAutos(carroSeleccionado, carrosVendidos, numCarrosVendidos, compradores, numCompradores);
+                    break;
+                }
+            }
+            if (truE == 1) {
+                break;
+            }
+        } while (true);
+        return false;
+    }
+
+    public static void registroAutos(Carro carro, Carro[] carrosVendidos, int numCarrosVendidos, Carro[] compradores, int numCompradores) {
+        carro.setNumeroControl(array[inicio]);
+        carro.setColor(color[inicio]);
+        carro.setMarca(marca[inicio]);
+        carro.setModelo(modelo[inicio]);
+        carro.setMonto(monto[inicio]);
+
+        for (int i = inicio; i < valores; i++) {
+            array[i] = array[i + 1];
+            color[i] = color[i + 1];
+            marca[i] = marca[i + 1];
+            modelo[i] = modelo[i + 1];
+            monto[i] = monto[i + 1];
+        }
+
+        array[valores] = 0;
+        color[valores] = null;
+        marca[valores] = null;
+        modelo[valores] = null;
+        monto[valores] = 0;
+        valores--;
+
+        carrosVendidos[numCarrosVendidos] = new GuiPrototype().new Carro();
+        carrosVendidos[numCarrosVendidos].setNumeroControl(carro.getNumeroControl());
+        carrosVendidos[numCarrosVendidos].setColor(carro.getColor());
+        carrosVendidos[numCarrosVendidos].setMarca(carro.getMarca());
+        carrosVendidos[numCarrosVendidos].setModelo(carro.getModelo());
+        carrosVendidos[numCarrosVendidos].setMonto(carro.getMonto());
+        compradores[numCompradores] = new GuiPrototype().new Carro();
+        compradores[numCompradores].setMarca(carro.getMarca());
+        compradores[numCompradores].setModelo(carro.getModelo());
+    }
+
+    public static void datos(int i, Carro carro) {
+        double a, b, c;
+
+        nombreComprador[i] = JOptionPane.showInputDialog(null, "Ingresa el nombre del comprador: ");
+
+        while (true) {
+            edad[i] = 0;
+            String str = JOptionPane.showInputDialog(null, "Ingresa la edad del comprador: ");
+
+            try {
+                edad[i] = Integer.parseInt(str);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada no válida. Ingresa un número válido para la edad.");
+                continue;
+            }
+
+            if (edad[i] < 18 || edad[i] > 100) {
+                JOptionPane.showMessageDialog(null, "El cliente no puede ser menor de edad o tener más de 100 años");
+            } else {
+                break;
+            }
+        }
+
+        metodoPago[i] = JOptionPane.showInputDialog(null, "Ingresa el método de pago: ");
+
+        while (true) {
+            b = 0;
+            
+            String str2 = JOptionPane.showInputDialog(null, String.format("Defina el porcentaje de enganche, IVA incluido con el monto del carro: $%d", carro.monto) + "\nSolo puede seleccionar porcentajes entre el 20 y 80 porciento, en caso de pago de contado ingrese 100: ");
+
+            try {
+                b = Double.parseDouble(str2);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada no válida. Ingresa un número válido para el porcentaje de enganche.");
+                continue;
+            }
+
+            if (b >= 20 && b <= 80) {
+                break;
+            }
+            if (b == 100) {
+                break;
+            }
+        }
+
+        a = b / 100;
+        montoEnganche[i] = a * carro.monto;
+        adeudo[i] = carro.monto - montoEnganche[i];
+        liquidacion[i] = carro.monto;
+
+        if (b >= 20 && b <= 80) {
+            do {
+                mesesAdeudo[i] = 0;
+                String str3 = JOptionPane.showInputDialog(null, "A cuántos años se pagará el adeudo? (max. 10 años): ");
+
+                try {
+                    mesesAdeudo[i] = Integer.parseInt(str3);
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,"Entrada no válida. Ingresa un número válido para el plazo de pago.");
+                    continue;
+                }
+
+                if (mesesAdeudo[i] < 10 && mesesAdeudo[i] > 0) {
+                    break;
+                }
+
+            } while (true);
+
+            c = mesesAdeudo[i] * 12;
+            pagoPorMes[i] = adeudo[i] / c;
+            JOptionPane.showMessageDialog(null, String.format("Mensualidad: %.2f",pagoPorMes[i])); 
+        }
+
+    }
 
     public GuiPrototype() {
         setLayout(null);
@@ -66,18 +334,16 @@ public class GuiPrototype extends JFrame implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
-        // Personalizar la apariencia del JOptionPane
         UIManager.put("OptionPane.background", Color.BLACK);
         UIManager.put("Panel.background", Color.BLACK);
         UIManager.put("OptionPane.messageForeground", Color.WHITE);
 
-        // Personalizar el aspecto del botón "OK" en todos los JOptionPanes
         int espacioInterno = 3;
         Border buttonBorder = new CompoundBorder(new LineBorder(Color.RED), new EmptyBorder(espacioInterno, espacioInterno, espacioInterno, espacioInterno));
         UIManager.put("Button.border", new BorderUIResource(buttonBorder));
-        UIManager.put("Button.background", new Color(0, 0, 0));  // Fondo negro
-        UIManager.put("Button.foreground", new Color(255, 255, 255));  // Texto blanco
-        UIManager.put("Button.focus", false);  // No mostrar foco
+        UIManager.put("Button.background", new Color(0, 0, 0));
+        UIManager.put("Button.foreground", new Color(255, 255, 255));
+        UIManager.put("Button.focus", false);
 
         if (e.getSource() == btn) {
             char[] passwordChars = jPasswordField1.getPassword();
@@ -118,7 +384,6 @@ public class GuiPrototype extends JFrame implements ActionListener{
     }
 
     private static void openNewWindow() {
-        GuiPrototype frame = new GuiPrototype();
         JFrame newFrame = new JFrame("Concesionaria");
 
         newFrame.setLayout(new BorderLayout());
@@ -128,42 +393,37 @@ public class GuiPrototype extends JFrame implements ActionListener{
         });
 
         JPanel contentPanel = new JPanel();
-        JButton boton = new JButton("Hola");
-        JButton boton2 = new JButton("nop");
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 
-        contentPanel.setLayout(null);
+        String[] moDaStr = mostrarDatos();
 
-        boton.setBounds(0, 0, 150, 30);
-        boton.setFocusPainted(false);
-        contentPanel.add(boton);
-        boton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent y) {
-                frame.i++;
-                System.out.println("¡Se hizo clic en el botón: " + frame.i);
-            }
-        });
+        for (int h = 0; h < 10; h++) {
+            // Crear un JPanel para agrupar el JLabel y el JButton
+            JPanel panel = new JPanel(new BorderLayout());
+            panel.setBorder(BorderFactory.createEmptyBorder(50, 0, 0, 0)); // Espaciado vertical de 50
 
-        boton2.setBounds(0, 250, 150, 30);
-        boton2.setFocusPainted(false);
-        contentPanel.add(boton2);
-        boton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent z) {
-                System.out.println("¡Se hizo clic en el botón!");
-            }
-        });
+            JLabel label = new JLabel("<html>" + moDaStr[h].replace("\n", "<br>") + "</html>");
+            label.setPreferredSize(new Dimension(300, 300));
+            label.setFont(new Font("Arial", Font.PLAIN, 25)); // Ajustar la fuente y el tamaño
+            panel.add(label, BorderLayout.CENTER);
+
+            JButton button = new JButton(new ImageIcon(obtenerImagen(h)));
+            button.setPreferredSize(new Dimension(300, 300));
+            button.addActionListener(new BotonListener(button, label, h));
+            button.setVerticalTextPosition(SwingConstants.BOTTOM);
+            button.setHorizontalTextPosition(SwingConstants.CENTER);
+            panel.add(button, BorderLayout.EAST);
+
+            contentPanel.add(panel);
+        }
 
         JScrollPane scrollPane = new JScrollPane(contentPanel);
+        scrollPane.getViewport().setBackground(Color.BLACK); 
+        scrollPane.setBorder(BorderFactory.createLineBorder(Color.RED));
 
-        // Personalizar el aspecto del JScrollPane
-        scrollPane.getViewport().setBackground(Color.BLACK);  // Fondo del área visible
-        scrollPane.setBorder(null);  // Borde del JScrollPane
-
-        // Cambiar el color del JScrollBar
         scrollPane.getVerticalScrollBar().setBackground(Color.BLACK);
+        scrollPane.getHorizontalScrollBar().setBackground(Color.BLACK);
 
-        // Cambiar el color del thumb (la parte móvil del JScrollBar)
         scrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
             @Override
             protected void configureScrollBarColors() {
@@ -186,6 +446,7 @@ public class GuiPrototype extends JFrame implements ActionListener{
                 return button;
             }
         });
+
         scrollPane.getHorizontalScrollBar().setUI(new BasicScrollBarUI() {
             @Override
             protected void configureScrollBarColors() {
@@ -198,6 +459,7 @@ public class GuiPrototype extends JFrame implements ActionListener{
                 button.setBackground(Color.BLACK);
                 button.setForeground(Color.RED);
                 button.setFocusPainted(false);
+                button.setBorder(null);
                 return button;
             }
 
@@ -207,18 +469,55 @@ public class GuiPrototype extends JFrame implements ActionListener{
                 button.setBackground(Color.BLACK);
                 button.setForeground(Color.RED);
                 button.setFocusPainted(false);
+                button.setBorder(null);
                 return button;
             }
         });
 
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
+        scrollPane.getVerticalScrollBar().setUnitIncrement(15);
+        scrollPane.getHorizontalScrollBar().setUnitIncrement(15);
         newFrame.add(scrollPane, BorderLayout.CENTER);
 
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         newFrame.setResizable(false);
         newFrame.setSize(1920, 1080);
         newFrame.setVisible(true);
+    }
+
+    static class BotonListener implements ActionListener {
+        private JButton button;
+        private JLabel associatedLabel;
+        private int ho;
+
+        public BotonListener(JButton button, JLabel associatedLabel, int ho) {
+            this.button = button;
+            this.associatedLabel = associatedLabel;
+            this.ho = ho;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            boolean confirm;
+            int w = 0;
+            Carro carroSeleccionado = new GuiPrototype().new Carro();
+            Carro[] carrosVendidos = new Carro[10];
+            Carro[] compradores = new Carro[10];
+            int numCompradores = 0;
+            int numCarrosVendidos = 0;
+            
+            confirm = buscar(carroSeleccionado, carrosVendidos, numCarrosVendidos, compradores, numCompradores, array[ho]);
+            datos(w, carroSeleccionado);
+            numCompradores++;
+            numCarrosVendidos++;    
+            w++;
+            button.setVisible(confirm);
+            associatedLabel.setVisible(confirm);
+        }
+    }
+
+    private static Image obtenerImagen(int index) {
+        //return new ImageIcon("carro_" + index + ".jpg").getImage();
+        return new ImageIcon("carro_0.jpg").getImage();
     }
 
     public static void main(String args[]) {
